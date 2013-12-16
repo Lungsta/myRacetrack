e873da657a0f9dc01147263f6f5f49dacef3d367
Lungelo <Lungelo@ndaba>
2013-12-16 13:33:30 +0200

    first

---
+++ racetrack/test/unit/racetrack/RaceTests.groovy
@@ -0,0 +1,28 @@
+package racetrack
+
+
+
+import grails.test.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+//@TestFor(Race)
+class RaceTests extends GrailsUnitTestCase {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+	
+	protected void setUp() {
+		super.setUp()
+		}
+	protected void tearDown() {
+		super.tearDown()
+		}
+	void testInMiles(){
+			def race = new Race(distance: 5.0)
+			assertEquals 3.107, race.inMIles()
+		}
+}
